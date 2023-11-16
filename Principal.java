ea5a857593d36d0e7e317619e1c3ae70cf50d590
PablorellanaM <139936006+PablorellanaM@users.noreply.github.com>
2023-11-15 23:45:30 -0600

    Add files via upload

--- Principal.java
+++ Principal.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 abstract class Usuario {
     protected String usuario;
@@ -30,6 +29,14 @@ abstract class Usuario {
 
     public boolean autenticar(String contrasena) {
         return this.contrasena.equals(contrasena);
+    }
+
+    public void cambiarContrasena(String nuevaContrasena) {
+        this.contrasena = nuevaContrasena;
+    }
+
+    public void cambiarTipoCliente() {
+        this.esPremium = !this.esPremium;
     }
 
     abstract void realizarReserva(Scanner scanner, Kayac kayak);
@@ -85,26 +92,6 @@ class Reserva {
         this.usuario = usuario;
     }
 
-    public String getFechaVuelo() {
-        return fechaVuelo;
-    }
-
-    public boolean isIdaYVuelta() {
-        return idaYVuelta;
-    }
-
-    public int getCantidadBoletos() {
-        return cantidadBoletos;
-    }
-
-    public String getAerolinea() {
-        return aerolinea;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
     public void imprimirConfirmacion() {
         System.out.println("Confirmación de Reserva:");
         System.out.println("Usuario: " + usuario.getUsuario());
@@ -117,7 +104,7 @@ class Reserva {
 
 class Kayac {
     private static final String USERS_FILE = "usuarios.txt";
-    private static final String RESERVAS_FILE = "reservas.csv";
+    private static final String RESERVAS_FILE = "usuarios.csv";
     private List<Usuario> usuarios = new ArrayList<>();
     private List<Reserva> reservas = new ArrayList<>();
 
@@ -158,18 +145,59 @@ class Kayac {
     }
 
     public void confirmarReserva(Scanner scanner, Usuario usuario) {
-        System.out.println("Reserva confirmada para " + usuario.getUsuario());
+        System.out.println("Modo confirmación:");
+        System.out.print("Ingrese el número de tarjeta: ");
+        String numeroTarjeta = scanner.nextLine();
+
+        int cuotas = 1;
+        if (!usuario.esPremium) {
+            System.out.print("Defina la cantidad de cuotas (1 a 24): ");
+            cuotas = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+        String claseVuelo = "Coach";
+        if (!usuario.esPremium) {
+            System.out.print("Defina la clase para el vuelo (Coach/Primera Clase): ");
+            claseVuelo = scanner.nextLine();
+        }
+
+        int numeroAsiento = 0;
+        int cantidadMaletas = 0;
+        if (usuario.esPremium) {
+            System.out.print("Seleccione el número de asiento: ");
+            numeroAsiento = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.print("Defina la cantidad de maletas: ");
+            cantidadMaletas = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+        imprimirItinerario(usuario, numeroTarjeta, cuotas, claseVuelo, numeroAsiento, cantidadMaletas);
+    }
+
+    private void imprimirItinerario(Usuario usuario, String numeroTarjeta, int cuotas, String claseVuelo,
+            int numeroAsiento, int cantidadMaletas) {
+        System.out.println("\nItinerario de Reserva:");
+        System.out.println("Usuario: " + usuario.getUsuario());
+        System.out.println("Número de Tarjeta: " + numeroTarjeta);
+        System.out.println("Cuotas: " + cuotas);
+        System.out.println("Clase de Vuelo: " + claseVuelo);
+        if (usuario.esPremium) {
+            System.out.println("Número de Asiento: " + numeroAsiento);
+            System.out.println("Cantidad de Maletas: " + cantidadMaletas);
+        }
     }
 
     public void guardarUsuarios() {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(USERS_FILE))) {
             for (Usuario usuario : usuarios) {
                 writer.write(usuario.getUsuario() + "," + usuario.getContrasena() + ","
-                        + (usuario.esPremium ? "premium" : "gratis"));
-                writer.newLine();
+                        + (usuario.esPremium ? "premium" : "gratis") + "\n");
             }
         } catch (IOException e) {
-            System.out.println("Error al escribir en el archivo: " + e.getMessage());
+            System.out.println("Error al guardar usuarios: " + e.getMessage());
         }
     }
 
@@ -178,35 +206,29 @@ class Kayac {
         if (!file.exists()) {
             return;
         }
-
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String linea;
-            while ((linea = reader.readLine()) != null) {
-                String[] datos = linea.split(",");
-                if (datos.length == 3) {
-                    Usuario usuario = datos[2].equals("premium") ? new UsuarioPremium(datos[0], datos[1])
-                            : new UsuarioGratis(datos[0], datos[1]);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 3) {
+                    Usuario usuario = "premium".equals(parts[2])
+                            ? new UsuarioPremium(parts[0], parts[1])
+                            : new UsuarioGratis(parts[0], parts[1]);
                     usuarios.add(usuario);
                 }
             }
         } catch (IOException e) {
-            System.out.println("Error al leer el archivo: " + e.getMessage());
+            System.out.println("Error al cargar usuarios: " + e.getMessage());
         }
     }
 
     private void guardarReservas() {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(RESERVAS_FILE))) {
             for (Reserva reserva : reservas) {
-                writer.write(
-                        reserva.getUsuario().getUsuario() + "," +
-                                reserva.getFechaVuelo() + "," +
-                                reserva.isIdaYVuelta() + "," +
-                                reserva.getCantidadBoletos() + "," +
-                                reserva.getAerolinea());
-                writer.newLine();
+                writer.write(reserva.toString() + "\n");
             }
         } catch (IOException e) {
-            System.out.println("Error al escribir en el archivo de reservas: " + e.getMessage());
+            System.out.println("Error al guardar reservas: " + e.getMessage());
         }
     }
 
@@ -215,30 +237,14 @@ class Kayac {
         if (!file.exists()) {
             return;
         }
-
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String linea;
-            while ((linea = reader.readLine()) != null) {
-                String[] datos = linea.split(",");
-                if (datos.length == 5) {
-                    Usuario usuario = buscarUsuario(datos[0]);
-                    if (usuario != null) {
-                        Reserva reserva = new Reserva(datos[1], Boolean.parseBoolean(datos[2]),
-                                Integer.parseInt(datos[3]), datos[4], usuario);
-                        reservas.add(reserva);
-                    }
-                }
+            String line;
+            while ((line = reader.readLine()) != null) {
+
             }
         } catch (IOException e) {
-            System.out.println("Error al leer el archivo de reservas: " + e.getMessage());
+            System.out.println("Error al cargar reservas: " + e.getMessage());
         }
-    }
-
-    private Usuario buscarUsuario(String nombreUsuario) {
-        return usuarios.stream()
-                .filter(u -> u.getUsuario().equals(nombreUsuario))
-                .findFirst()
-                .orElse(null);
     }
 }
 
@@ -249,9 +255,9 @@ public class Principal {
     public static void main(String[] args) {
         boolean salir = false;
         while (!salir) {
-            System.out.println("\nBienvenido al sistema de reservas Kayac.");
+            System.out.println("Bienvenido a Kayac");
             System.out.println("1. Registrarse");
-            System.out.println("2. Ingresar");
+            System.out.println("2. Iniciar sesión");
             System.out.println("3. Salir");
             System.out.print("Seleccione una opción: ");
             int opcion = scanner.nextInt();
@@ -262,75 +268,60 @@ public class Principal {
                     registrarUsuario();
                     break;
                 case 2:
-                    Usuario usuario = autenticarUsuario();
-                    if (usuario != null) {
-                        System.out.println("Autenticación exitosa. Bienvenido " + usuario.getUsuario());
-                        manejarAccionesUsuario(usuario);
-                    }
+                    iniciarSesion();
                     break;
                 case 3:
                     salir = true;
-                    kayak.guardarUsuarios();
-                    System.out.println("Gracias por usar el sistema de reservas Kayac.");
                     break;
                 default:
                     System.out.println("Opción no válida.");
-                    break;
             }
         }
-        scanner.close();
     }
 
     private static void registrarUsuario() {
-        System.out.print("Ingrese un nombre de usuario: ");
+        System.out.print("Ingrese su nombre de usuario: ");
         String usuario = scanner.nextLine();
-        System.out.print("Ingrese una contraseña: ");
+        System.out.print("Ingrese su contraseña: ");
         String contrasena = scanner.nextLine();
-        System.out.print("¿Desea registrarse como usuario Premium? (si/no): ");
-        String respuesta = scanner.nextLine();
+        System.out.print("¿Desea ser usuario premium? (si/no): ");
+        boolean esPremium = "si".equalsIgnoreCase(scanner.nextLine());
 
-        if ("si".equalsIgnoreCase(respuesta)) {
-            kayak.registroUsuario(new UsuarioPremium(usuario, contrasena));
-            System.out.println("Usuario Premium registrado exitosamente.");
-        } else {
-            kayak.registroUsuario(new UsuarioGratis(usuario, contrasena));
-            System.out.println("Usuario registrado exitosamente como usuario gratis.");
-        }
+        Usuario nuevoUsuario = esPremium
+                ? new UsuarioPremium(usuario, contrasena)
+                : new UsuarioGratis(usuario, contrasena);
+        kayak.registroUsuario(nuevoUsuario);
+        System.out.println("Usuario registrado exitosamente.");
     }
 
-    private static Usuario autenticarUsuario() {
+    private static void iniciarSesion() {
         System.out.print("Ingrese su nombre de usuario: ");
         String usuario = scanner.nextLine();
         System.out.print("Ingrese su contraseña: ");
         String contrasena = scanner.nextLine();
 
         Usuario usuarioAutenticado = kayak.autenticarUsuario(usuario, contrasena);
-        if (usuarioAutenticado == null) {
-            System.out.println("Autenticación fallida.");
-        }
-        return usuarioAutenticado;
-    }
-
-    private static void manejarAccionesUsuario(Usuario usuario) {
-        if (usuario.esPremium) {
-            mostrarMenuPremium(usuario);
+        if (usuarioAutenticado != null) {
+            System.out.println("Inicio de sesión exitoso.");
+            menuUsuario(usuarioAutenticado);
         } else {
-            mostrarMenuGratis(usuario);
+            System.out.println("Usuario o contraseña incorrectos.");
         }
     }
 
-    private static void mostrarMenuGratis(Usuario usuario) {
-        boolean continuar = true;
-        while (continuar) {
-            System.out.println("\nMenú de Usuario Gratis");
+    private static void menuUsuario(Usuario usuario) {
+        boolean salir = false;
+        while (!salir) {
+            System.out.println("Bienvenido, " + usuario.getUsuario());
             System.out.println("1. Realizar reserva");
             System.out.println("2. Confirmar reserva");
-            System.out.println("3. Salir");
-            System.out.print("Elija una opción: ");
-            int eleccion = scanner.nextInt();
+            System.out.println("3. Modo perfil");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
+            int opcion = scanner.nextInt();
             scanner.nextLine();
 
-            switch (eleccion) {
+            switch (opcion) {
                 case 1:
                     usuario.realizarReserva(scanner, kayak);
                     break;
@@ -338,40 +329,47 @@ public class Principal {
                     usuario.confirmarReserva(scanner, kayak);
                     break;
                 case 3:
-                    continuar = false;
+                    modoPerfil(usuario);
+                    break;
+                case 4:
+                    salir = true;
                     break;
                 default:
-                    System.out.println("Opción no válida");
-                    break;
+                    System.out.println("Opción no válida.");
             }
         }
     }
 
-    private static void mostrarMenuPremium(Usuario usuario) {
-        boolean continuar = true;
-        while (continuar) {
-            System.out.println("\nMenú de Usuario Premium");
-            System.out.println("1. Realizar reserva");
-            System.out.println("2. Confirmar reserva");
-            System.out.println("3. Salir");
-            System.out.print("Elija una opción: ");
-            int eleccion = scanner.nextInt();
-            scanner.nextLine();
+    private static void modoPerfil(Usuario usuario) {
+        System.out.println("Modo perfil");
+        System.out.println("1. Modificar tipo de cliente");
+        System.out.println("2. Aplicar cupón de descuento");
+        System.out.println("3. Cambiar contraseña");
+        System.out.print("Seleccione una opción: ");
+        int opcion = scanner.nextInt();
+        scanner.nextLine();
 
-            switch (eleccion) {
-                case 1:
-                    usuario.realizarReserva(scanner, kayak);
-                    break;
-                case 2:
-                    usuario.confirmarReserva(scanner, kayak);
-                    break;
-                case 3:
-                    continuar = false;
-                    break;
-                default:
-                    System.out.println("Opción no válida");
-                    break;
-            }
+        switch (opcion) {
+            case 1:
+                usuario.cambiarTipoCliente();
+                System.out.println("Tipo de cliente modificado.");
+                break;
+            case 2:
+                if (!usuario.esPremium) {
+                    System.out.println("Aplicando cupón de 10% de descuento...");
+
+                } else {
+                    System.out.println("Los usuarios premium no necesitan cupones de descuento.");
+                }
+                break;
+            case 3:
+                System.out.print("Ingrese su nueva contraseña: ");
+                String nuevaContrasena = scanner.nextLine();
+                usuario.cambiarContrasena(nuevaContrasena);
+                System.out.println("Contraseña cambiada exitosamente.");
+                break;
+            default:
+                System.out.println("Opción no válida.");
         }
     }
 }
